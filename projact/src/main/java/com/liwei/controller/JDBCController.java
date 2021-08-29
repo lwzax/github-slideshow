247e96b2653980de44f96dfad5d7900ee5c6aade
lwzax <87065467+lwzax@users.noreply.github.com>
2021-08-29 22:39:35 +0800

    Add files via upload

---
+++ projact/src/main/java/com/liwei/controller/JDBCController.java
@@ -0,0 +1,23 @@
+package com.liwei.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+public class JDBCController {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/userList")
+    public List<Map<String,Object>> userList(){
+        String sql = "select * from user";
+        List<Map<String, Object>> list_maps = jdbcTemplate.queryForList(sql);
+        return list_maps;
+    }
+
+}
---
+++ projact/src/main/java/com/liwei/service/UserService.java
@@ -0,0 +1,9 @@
+package com.liwei.service;
+
+import com.liwei.pojo.User;
+
+import java.util.List;
+
+public interface UserService {
+    public List<User> SelectUser();
+}
